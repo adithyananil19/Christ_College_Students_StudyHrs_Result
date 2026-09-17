491a513f8f56f3dbf4a3d57f8041fa05236771f2
P A Adithyan <adithyananil19@gmail.com>
2026-09-17 09:29:09 +0530

    attribute updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(__file__).parent / "student_pass_fail_model.pkl"
+model_path = Path(__file__).parent / "logistic_regression_model.pkl"
 model = joblib.load(model_path)
 
 st.title("Student Pass Predictor")
